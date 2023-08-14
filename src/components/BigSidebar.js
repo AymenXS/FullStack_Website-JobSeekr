26c63e406e042a6fffceec5c93e467333bdf203c
AymenXS <aymenghaloua@gmail.com>
2023-08-14 14:34:21 +0100

    UPDATING: Sidebars and Links

--- src/components/BigSidebar.js
+++ src/components/BigSidebar.js
@@ -1,9 +1,28 @@
-import Wrapper from "../assets/wrappers/BigSidebar"
+import NavLinks from './NavLinks';
+import Logo from '../components/Logo';
+import Wrapper from '../assets/wrappers/BigSidebar';
+import { useSelector } from 'react-redux';
 
 const BigSidebar = () => {
-
+  const { isSidebarOpen } = useSelector((store) => store.user);
   return (
-    <Wrapper>BigSidebar</Wrapper>
-  )
-}
-export default BigSidebar
+    <Wrapper>
+      <div
+        className={
+          isSidebarOpen
+            ? 'sidebar-container show-sidebar'
+            : 'sidebar-container '
+        }
+      >
+        <div className='content'>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default BigSidebar;
