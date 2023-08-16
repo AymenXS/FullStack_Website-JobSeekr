d04b5e5e0cc3a5a51b414efbb35707c7ab17f338
AymenXS <aymenghaloua@gmail.com>
2023-08-16 22:18:08 +0100

    ADDING: AllJobs Components

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,8 @@ import FormRow from '../components/FormRow'
 import Navbar from './Navbar';
 import SmallSidebar from './SmallSidebar';
 import BigSidebar from './BigSidebar';
+import Job from './Job';
+import JobsContainer from './JobsContainer';
+import SearchContainer from './SearchContainer';
 
-export { Logo, FormRow, BigSidebar, SmallSidebar, Navbar }
+export { Logo, FormRow, BigSidebar, SmallSidebar, Navbar, Job, JobsContainer, SearchContainer }
